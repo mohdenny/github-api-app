7b10416cc5a1c1793a4c9ef755575f0dbab4850b
Mohammad Denny <mohammaddenny94@gmail.com>
2021-08-26 04:13:00 +0700

    import React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+
+// Redux 
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <div className="">Hello</div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+            <section className='container mx-auto p-8'>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/:id" component={Dashboard} />
+              </Switch>
+            </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
