d5c5651bd3ebb3a69eb4baf51a5b5181ede2da00
Mohammad Denny <mohammaddenny94@gmail.com>
2021-08-26 16:07:31 +0700

    import SearchBar

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,12 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import ReposList from '../dashboard/ReposList';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import SearchBar from '../layout/SearchBar';
 import { getGithubRepos } from '../../actions/github';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const Dashboard = ({ github: { repos }, getGithubRepos, match }) => {
     const [username, setUsername] = useState('mohdenny');
-    const [newRepo, setNewRepo] = useState();
 
     useEffect(() => {
         if(match.params.id) {
@@ -17,28 +16,10 @@ const Dashboard = ({ github: { repos }, getGithubRepos, match }) => {
         getGithubRepos(username);
     }, [getGithubRepos, match.params.id, username]);
 
-    const fetchMoreData = () => {
-        // a fake async api call like which sends
-        // 20 more records in 1.5 secs
-        setTimeout(() => {
-            setNewRepo(repos.concat(Array.from({ length: 5 })));
-            return newRepo;
-        }, 1500);
-    };
-
-    console.log(repos);
-    console.log(newRepo);
-
     return (
         <Fragment>
-            <InfiniteScroll
-                dataLength={repos.length}
-                next={fetchMoreData}
-                hasMore={true}
-                loader={<h4>Loading...</h4>}
-            >
-                    <ReposList repos={repos}/>
-            </InfiniteScroll>
+            <SearchBar />
+            <ReposList repos={repos} />
         </Fragment>
     )
 };
