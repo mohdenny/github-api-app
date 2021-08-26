2916d5f698d5fdfae15b0b6a914f71ef3472f5d6
Mohammad Denny <mohammaddenny94@gmail.com>
2021-08-26 16:07:11 +0700

    add pagination

--- src/components/dashboard/ReposList.js
+++ src/components/dashboard/ReposList.js
@@ -1,66 +1,67 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const ReposList = ({ repos }) => {
-    const renderedList = repos.map(repo => {
+    const initialState = {
+        visible: 2,
+    }
+
+    const [state, setState] = useState(initialState);
+
+    const loadMore = () => {
+        setState({visible: state.visible + 4});
+    }
+
+    console.log(repos);
+
+    const renderedList = repos.slice(0, state.visible).map((repo => {
         return (
-            <tr key={repo.id}>
-                <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                    <div className="flex-shrink-0 h-10 w-10">
-                        <a href={repo.owner.avatar_url} target="_blank" rel='noopener noreferrer'>
-                            <img className="h-10 w-10 rounded-full hover:opacity-50" src={repo.owner.avatar_url} alt={repo.owner.login} />
-                        </a>
-                    </div>
-                    <div className="ml-4">
-                        <div className="text-sm font-medium text-gray-900 hover:text-gray-500">
-                            <a href={repo.owner.html_url} target="_blank" rel='noopener noreferrer'>
-                                {repo.owner.login}
-                            </a>
-                        </div>
-                        <div className="text-sm text-gray-500">
-                            {repo.owner.html_url}
-                        </div>
-                    </div>
-                    </div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-900 hover:text-gray-500">
-                        <a href={repo.html_url} target="_blank" rel='noopener noreferrer'>    
-                            {repo.name}
-                        </a>
-                    </div>
-                    <div className="text-sm text-gray-500 ">  
-                        {repo.html_url}
-                    </div>
-                </td>
-            </tr>
+            <div key={repo.id} className="p-2 grid grid-flow-col auto-cols-max gap-4 h-16 border rounded-lg shadow mb-4">
+                <div className="flex flex-col justify-center items-center">
+                    <a href={repo.owner.avatar_url} target="_blank" rel='noopener noreferrer'>
+                        <img className="h-10 w-10 rounded-full hover:opacity-50" src={repo.owner.avatar_url} alt={repo.owner.login} />
+                    </a>
+                </div>
+                <div className="flex flex-col justify-center items-center font-medium text-gray-900 text-sm hover:text-gray-500">
+                    <a href={repo.owner.html_url} target="_blank" rel='noopener noreferrer'>
+                        {repo.owner.login}
+                    </a>
+                </div>
+                <div className="flex flex-col justify-center items-center text-gray-900 text-sm hover:text-gray-500">
+                    <a href={repo.html_url} target="_blank" rel='noopener noreferrer'>    
+                        {repo.name}
+                    </a>
+                </div>
+                <div className="flex flex-col justify-center items-center text-gray-900 text-sm">
+                    Stars: {repo.stargazers_count}
+                </div>
+                <div className="flex flex-col justify-center items-center text-gray-900 text-sm">
+                    Watchers: {repo.watchers_count}
+                </div>
+                <div className="flex flex-col justify-center items-center text-gray-900 text-sm">
+                    Forks: {repo.forks_count}
+                </div>
+            </div>
         )
-    });
+    }));
 
     return (
         <Fragment>
-            <div className="flex flex-col">
-                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-200">
-                                <thead className="bg-gray-50">
-                                    <tr>
-                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Name
-                                        </th>
-                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Repository
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {renderedList}
-                                </tbody>
-                            </table>
+            <div className="flex flex-col justify-center items-center">
+                {   
+                    repos && repos.length > 0 ?
+                        renderedList 
+                        
+                    :
+                        <div className="py-2 h-screen">
+                            <div className="flex flex-col justify-center items-center h-full w-full">
+                                <div className="italic text-sm">No results</div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                }
+
+                {   
+                    state.visible < repos.length && <button onClick={loadMore} type="button" className="load-more">Load more</button>
+                }
             </div>
         </Fragment>
     )
