4c4f58a7587b1cba19cdae44b40c5a9feef0c993
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-26 14:33:21 +0700

    add classname shadow

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Navbar = () => {
     return (
         <Fragment>
-            <nav className="bg-gray-100">
+            <nav className="bg-gray-100 shadow-lg">
                 <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                     <div className="flex flex-col justify-center items-center h-16">
                         <div className="font-bold text-2xl">GITREPOS</div>
