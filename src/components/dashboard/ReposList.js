c04c72b55eb4329ed5ee63a6254fe5e7b0b9e1b5
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-26 14:33:12 +0700

    change classname grid to flex

--- src/components/dashboard/ReposList.js
+++ src/components/dashboard/ReposList.js
@@ -15,30 +15,30 @@ const ReposList = ({ repos }) => {
 
     const renderedList = repos.slice(0, state.visible).map((repo => {
         return (
-            <div key={repo.id} className="p-2 grid grid-cols-6 justify-items-stretch gap-4 h-16 border rounded-lg w-full max-w-2xl shadow mb-4">
-                <div className="flex flex-col justify-center items-center px-2">
+            <div key={repo.id} className="p-2 flex items-center justify-between h-16 border rounded-lg w-full max-w-2xl shadow-lg space-x-4 bg-white">
+                <div className="flex justify-center items-center h-full w-full px-2">
                     <a href={repo.owner.avatar_url} target="_blank" rel='noopener noreferrer'>
                         <img className="h-10 w-10 rounded-full hover:opacity-50" src={repo.owner.avatar_url} alt={repo.owner.login} />
                     </a>
                 </div>
-                <div className="flex flex-col justify-center items-center px-2 font-medium text-gray-900 text-sm hover:text-gray-500">
+                <div className="flex justify-center items-center h-full w-full px-2 font-medium text-gray-900 text-sm hover:text-gray-500">
                     <a href={repo.owner.html_url} target="_blank" rel='noopener noreferrer'>
-                        {repo.owner.login}
+                        <p>{repo.owner.login}</p>
                     </a>
                 </div>
-                <div className="flex flex-col justify-center items-center px-2 text-gray-900 text-sm hover:text-gray-500">
+                <div className="flex justify-center items-center h-full w-full px-2 text-gray-900 text-sm hover:text-gray-500">
                     <a href={repo.html_url} target="_blank" rel='noopener noreferrer'>    
-                        {repo.name}
+                        <p className="break-all">{repo.name}</p>
                     </a>
                 </div>
-                <div className="flex flex-col hidden sm:inline-block justify-center items-center px-2 text-gray-900 text-sm">
-                    Stars: {repo.stargazers_count}
+                <div className="sm:flex items-center justify-center hidden h-full w-full px-2 text-gray-900 text-sm">
+                    <p>Stars: {repo.stargazers_count}</p>
                 </div>
-                <div className="flex flex-col hidden sm:inline-block justify-center items-center px-2 text-gray-900 text-sm">
-                    Watchers: {repo.watchers_count}
+                <div className="sm:flex items-center justify-center hidden h-full w-full px-2 text-gray-900 text-sm">
+                    <p>Watchers: {repo.watchers_count}</p>
                 </div>
-                <div className="flex flex-col hidden sm:inline-block justify-center items-center px-2 text-gray-900 text-sm">
-                    Forks: {repo.forks_count}
+                <div className="sm:flex items-center justify-center hidden h-full w-full px-2 text-gray-900 text-sm">
+                    <p>Forks: {repo.forks_count}</p>
                 </div>
             </div>
         )
@@ -46,21 +46,21 @@ const ReposList = ({ repos }) => {
 
     return (
         <Fragment>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col space-y-4 justify-center items-center">
                 {   
                     repos && repos.length > 0 ?
                         renderedList 
                         
                     :
                         <div className="py-2 h-screen">
-                            <div className="flex flex-col justify-center items-center h-full w-full">
+                            <div className="flex justify-center items-center h-full w-full">
                                 <div className="italic text-sm">No results</div>
                             </div>
                         </div>
                 }
 
                 {   
-                    state.visible < repos.length && <button onClick={loadMore} type="button" className="load-more">Load more</button>
+                    state.visible < repos.length && <button onClick={loadMore} type="button" className="load-more p-2 rounded-xl border-gray-300 hover:bg-gray-300">Load more</button>
                 }
             </div>
         </Fragment>
